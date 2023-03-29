e99ed58d8f79f6c2b093d941905ae66ae9c5ec52
yeray-yas <yerayarucas@gmail.com>
2023-03-29 12:06:53 +0200

    new folders created

--- app/src/main/java/com/yerayyas/marvelsuperheroes/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/ui/main/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yerayyas.marvelsuperheroes
+package com.yerayyas.marvelsuperheroes.ui.main.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yerayyas.marvelsuperheroes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
