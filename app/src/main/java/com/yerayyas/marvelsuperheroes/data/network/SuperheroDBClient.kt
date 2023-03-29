e504798f4d4cd3f0f835e0e8a43799b734a87ae9
yeray-yas <yerayarucas@gmail.com>
2023-03-29 12:21:49 +0200

    retrofit client SuperheroDBClient added

---
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/data/network/SuperheroDBClient.kt
@@ -0,0 +1,17 @@
+package com.yerayyas.marvelsuperheroes.data.network
+
+import com.yerayyas.marvelsuperheroes.core.utls.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object SuperheroDbClient {  //This is a singleton. It will make only one retrofit instance
+
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+    val service: MarvelService = retrofit.create(MarvelService::class.java)
+}
