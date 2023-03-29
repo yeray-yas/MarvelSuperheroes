08186c316498a286bc86d6f4afeb66d8b72b8176
yeray-yas <yerayarucas@gmail.com>
2023-03-29 12:11:39 +0200

    Constants file created

---
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/core/utls/Constants.kt
@@ -0,0 +1,9 @@
+package com.yerayyas.marvelsuperheroes.core.utls
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    const val TS = 1
+    const val API_KEY = "3de6bbd5de0a40038da2c8fe677fb23b"
+    const val HASH = "56feb160b3d944895040bec40ead241b"
+
+}
