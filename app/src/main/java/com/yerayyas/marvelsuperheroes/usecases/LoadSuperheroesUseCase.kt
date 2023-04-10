641232bb83dd8763d918a76e39bd62713e38d80d
yeray-yas <yerayarucas@gmail.com>
2023-04-10 16:35:30 +0200

    deleted artificial delay

--- app/src/main/java/com/yerayyas/marvelsuperheroes/usecases/LoadSuperheroesUseCase.kt
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/usecases/LoadSuperheroesUseCase.kt
@@ -3,14 +3,12 @@ package com.yerayyas.marvelsuperheroes.usecases
 import com.yerayyas.marvelsuperheroes.data.model.Superhero
 import com.yerayyas.marvelsuperheroes.data.repositories.SuperheroRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 class LoadSuperheroesUseCase(private val repository: SuperheroRepository) {
 
     suspend fun invoke(): List<Superhero> = withContext(Dispatchers.IO) {
 
-        delay(2000)
         repository.getSuperheroes()
 
 
