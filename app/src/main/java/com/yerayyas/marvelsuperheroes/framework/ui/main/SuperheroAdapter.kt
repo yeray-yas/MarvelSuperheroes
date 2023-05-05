a8fe02e753313da519900805accb926b7165d96a
yeray-yas <yerayarucas@gmail.com>
2023-05-05 05:07:47 +0200

    extracted the Delegate to an Extension function

--- app/src/main/java/com/yerayyas/marvelsuperheroes/framework/ui/main/SuperheroAdapter.kt
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/framework/ui/main/SuperheroAdapter.kt
@@ -2,37 +2,21 @@ package com.yerayyas.marvelsuperheroes.framework.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.yerayyas.marvelsuperheroes.data.model.Superhero
 import com.yerayyas.marvelsuperheroes.databinding.ViewSuperheroItemBinding
-import kotlin.properties.Delegates
+import com.yerayyas.marvelsuperheroes.framework.ui.basicDiffUtil
 
 class SuperheroAdapter(
 
-    private val superheroClickedListener: (Superhero) -> Unit) :
+    private val superheroClickedListener: (Superhero) -> Unit
+) :
     RecyclerView.Adapter<SuperheroAdapter.ViewHolder>() {
 
-    var superheroes: List<Superhero> by Delegates.observable(emptyList()){ _, old, new ->
-        DiffUtil.calculateDiff(object: DiffUtil.Callback(){
-            override fun getOldListSize(): Int = old.size
-
-            override fun getNewListSize(): Int = new.size
-
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-                val oldItem = old[oldItemPosition]
-                val newItem = new[newItemPosition]
-
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-                return old[oldItemPosition] == new[newItemPosition]
-            }
-
-        }).dispatchUpdatesTo(this)
-    }
+    var superheroes: List<Superhero> by basicDiffUtil(
+        areItemsTheSame = { old, new -> old.id == new.id }
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
