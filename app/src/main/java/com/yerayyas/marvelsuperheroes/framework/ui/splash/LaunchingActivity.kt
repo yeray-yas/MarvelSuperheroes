8710d6ec84e8b7375a4e878db702cb01cf29ea75
yeray-yas <yerayarucas@gmail.com>
2023-05-12 15:12:54 +0200

    added Splash Screen

---
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/framework/ui/splash/LaunchingActivity.kt
@@ -0,0 +1,35 @@
+package com.yerayyas.marvelsuperheroes.framework.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.yerayyas.marvelsuperheroes.R
+import com.yerayyas.marvelsuperheroes.framework.ui.main.MainActivity
+
+
+class LaunchingActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launching)
+
+
+        val lottieAnimationView: LottieAnimationView = findViewById(R.id.lottieAnim)
+
+        lottieAnimationView.setAnimation(R.raw.superheroes_animation)
+        lottieAnimationView.playAnimation()
+
+
+        handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed(
+            {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000
+        )
+    }
+}
