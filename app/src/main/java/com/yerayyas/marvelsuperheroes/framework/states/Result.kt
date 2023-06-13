c14a012a4af90f5725b073c6abd159ea32349192
yeray-yas <yerayarucas@gmail.com>
2023-06-14 01:55:13 +0200

    handling errors implemented

---
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/framework/states/Result.kt
@@ -0,0 +1,14 @@
+package com.yerayyas.marvelsuperheroes.framework.states
+
+sealed class Result<out V, out E : Failure> {
+    object Loading : Result<Nothing, Nothing>()
+    data class Success<out V>(val value: V) : Result<V, Nothing>()
+    data class Error<out E : Failure>(val failure: E) : Result<Nothing, E>()
+
+
+    val isSuccess: Boolean
+        get() = this is Success<V>
+
+    val isError: Boolean
+        get() = this is Error<E>
+}
