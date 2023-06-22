019c0ee88e32eb87cca4bbbed5c15dcc1756c45a
yeray-yas <yerayarucas@gmail.com>
2023-06-22 20:48:14 +0100

    room's dao and entity

--- app/src/main/java/com/yerayyas/marvelsuperheroes/framework/data/database/dao/SuperheroDao.kt
+++ app/src/main/java/com/yerayyas/marvelsuperheroes/framework/data/database/dao/SuperheroDao.kt
@@ -2,6 +2,7 @@ package com.yerayyas.marvelsuperheroes.framework.data.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.yerayyas.marvelsuperheroes.framework.data.database.entities.SuperheroEntity
 
@@ -11,7 +12,7 @@ interface SuperheroDao {
     @Query("SELECT * FROM superhero_table ORDER BY name DESC")
     suspend fun getAllSuperheroes():List<SuperheroEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(superheroes:List<SuperheroEntity>)
 
 
